73b608edd6fe87f168cd2e76846b93f31af68da7
XJ Hsu <xj11400@gmail.com>
2025-12-25 07:28:04 +0800

    fix(flake): system url

--- flake.nix
+++ flake.nix
@@ -3,13 +3,12 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs?ref=nixos-unstable";
-    systems.url = "github:xj11400/flake-lib?dir=systems";
   };
 
   outputs = inputs: {
     lib = import ./lib {
-      pkgs = inputs.nixpkgs;
-      systems = import inputs.systems;
+      inherit (inputs) nixpkgs;
+      systems = import ./systems;
     };
   };
 }
